bfacfde12790910836cb97bbfe4094c1e3a0bad9
MrHunter <eduardoorsi2006@gmail.com>
2025-11-03 14:33:34 -0300

    ayuda

--- core/src/main/java/red/HiloCliente.java
+++ core/src/main/java/red/HiloCliente.java
@@ -1,5 +1,36 @@
 package red;
 
-public class HiloCliente extends Thread{
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
+public class HiloCliente extends Thread{
+	 	private DatagramSocket socket;
+	    private int servidorPort = 5555;
+	    private String ipServidorStr = "255.255.255.255";
+	    private InetAddress ipServidor;
+	    private boolean fin = false;
+	    
+	    public HiloCliente() {
+	        try {
+	        	this.ipServidor = InetAddress.getByName(this.ipServidorStr);
+	            this.socket = new DatagramSocket();
+	        } catch (SocketException | UnknownHostException e) {
+//	            throw new RuntimeException(e);
+	        }
+	    }
+	    @Override
+	    public void run() {
+	        do {
+	            DatagramPacket paquete = new DatagramPacket(new byte[1024], 1024);
+	            try {
+	                this.socket.receive(paquete);
+	            } catch (IOException e) {
+//	                throw new RuntimeException(e);
+	            }
+	        } while(!this.fin);
+	    }
 }
