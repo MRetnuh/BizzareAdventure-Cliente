04b2cce01703b5a8cd5a4cd76c0b1dfa6fec2f46
MejiaEynar <eynarmejia03@gmail.com>
2025-11-21 18:29:12 -0300

    Desconexion por servidor implementado

--- core/src/main/java/red/HiloCliente.java
+++ core/src/main/java/red/HiloCliente.java
@@ -1,142 +1,184 @@
-    package red;
+package red;
 
-    import java.io.IOException;
-    import java.net.DatagramPacket;
-    import java.net.DatagramSocket;
-    import java.net.InetAddress;
-    import java.net.SocketException;
-    import java.net.UnknownHostException;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
-    import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Gdx;
 
-    import interfaces.GameController;
+import interfaces.GameController;
 
-    public class HiloCliente extends Thread{
-            private DatagramSocket socket;
-            private int servidorPort = 5555;
-            private String ipServidorStr = "255.255.255.255";
-            private InetAddress ipServidor;
-            private boolean fin = false;
-            private GameController gameController;
-            private boolean conexionEntreJugadores = false;
-            public HiloCliente(GameController gameController) {
-                try {
-                    this.gameController = gameController;
-                    this.ipServidor = InetAddress.getByName(this.ipServidorStr);
-                    this.socket = new DatagramSocket();
-                } catch (SocketException | UnknownHostException e) {
-    //	            throw new RuntimeException(e);
-                }
-            }
-            @Override
-            public void run() {
-                do {
-                    DatagramPacket paquete = new DatagramPacket(new byte[1024], 1024);
-                    try {
-                        this.socket.receive(paquete);
-                        procesarMensaje(paquete);
-                    } catch (IOException e) {
-    //	                throw new RuntimeException(e);
-                    }
-                } while(!this.fin);
-            }
+public class HiloCliente extends Thread {
+    private DatagramSocket socket;
+    private int servidorPort = 5555;
+    private String ipServidorStr = "255.255.255.255";
+    private InetAddress ipServidor;
+    private boolean fin = false;
+    private GameController gameController;
+    private boolean conexionEntreJugadores = false;
 
-            private void procesarMensaje(DatagramPacket packet) {
-                String mensaje = (new String(packet.getData())).trim();
-                String[] partes = mensaje.split(":");
+    // Control de timeout
+    private long ultimaActividadServidor = System.currentTimeMillis();
+    private static final long TIMEOUT_SERVIDOR = 4000; // 4 segundos
 
-                System.out.println("Mensaje recibido: " + mensaje);
-
-                switch(partes[0]){
-                    case "Yaconectado":
-                        System.out.println("Ya estas conectado");
-                        break;
-                    case "Conectado":
-                        System.out.println("Conectado al servidor");
-                        this.ipServidor = packet.getAddress();
-                        gameController.conectar(Integer.parseInt(partes[1]));
-                        break;
-                    case "Lleno":
-                        System.out.println("Servidor lleno");
-                        this.fin = true;
-                        break;
-                    case "Empezar":
-                        this.conexionEntreJugadores = true;
-                        this.gameController.empezar(Integer.parseInt(partes[1]), Integer.parseInt(partes[2]));
-                        break;
-                    case "Nivel":
-                        this.gameController.asignarNivel(Integer.parseInt(partes[1]));
-                        break;
-                    case "UpdateState":
-                        this.gameController.actualizarEstado(partes);
-                        break;
-                    case "Derrota":
-                        this.gameController.perder();
-                        break;
-                    case "Animar":
-                        this.gameController.animar(Integer.parseInt(partes[1]), Boolean.parseBoolean(partes[2]),
-                        Boolean.parseBoolean(partes[3]), Boolean.parseBoolean(partes[4]));
-                        break;
-                    case "Atacar":
-                        this.gameController.animarPersonajeAtaque(partes);
-                        break;
-                    case "MovimientoEnemigos":
-                        this.gameController.actualizarMovimientoEnemigos(partes);
-                        break;
-                    case "BalasEnemigos":
-                        this.gameController.actualizarBalasEnemigos(partes);
-                        break;
-                    case "ActualizarBalas":
-                        this.gameController.actualizarPosicionBalas(partes);
-                        break;
-                    case "CajaRota":
-                        this.gameController.eliminarCaja(partes);
-                        break;
-                    case "BalaImpactada":
-                        this.gameController.eliminarBala(partes);
-                        break;
-                    case "CambioPersonaje":
-                        this.gameController.cambiarPersonaje(Integer.parseInt(partes[1]), Integer.parseInt(partes[2]));
-                        break;
-                    case "NivelCompletado":
-                        this.gameController.avanzarNivel(partes);
-                        break;
-                    case "CambioPersonajesNivel":
-                        this.gameController.cambiarPersonajesPorNivel(partes);
-                        break;
-                    case "Desconectado":
-                        this.gameController.volverAlMenu();
-                        break;
-                    case "Victoria":
-                        this.gameController.ganarPartida();
-                        break;
-                    case "ErrorJugador":
-                        System.out.println("El servidor ha desconectado al jugador.");
-                        this.gameController.tirarErrorPorDesconexion();
-                        break;
-
-                }
-
-            }
-
-            public void sendMessage(String message) {
-                byte[] byteMessage = message.getBytes();
-                DatagramPacket packet = new DatagramPacket(byteMessage, byteMessage.length, this.ipServidor, this.servidorPort);
-                try {
-                    this.socket.send(packet);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            public void finalizar() {
-                this.fin = true;
-                this.socket.close();
-                this.interrupt();
-                System.out.println("Conexion finalizada");
-            }
-
-            public boolean getConexionEntreJugadores() {
-                return this.conexionEntreJugadores;
-            }
+    public HiloCliente(GameController gameController) {
+        try {
+            this.gameController = gameController;
+            this.ipServidor = InetAddress.getByName(this.ipServidorStr);
+            this.socket = new DatagramSocket();
+        } catch (SocketException | UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
+
+    @Override
+    public void run() {
+        // Hilo principal que recibe mensajes
+        new Thread(this::verificarTimeout).start(); // Comienza el hilo de control de timeout
+
+        do {
+            DatagramPacket paquete = new DatagramPacket(new byte[1024], 1024);
+            try {
+                this.socket.receive(paquete);
+                procesarMensaje(paquete);  // Procesa el mensaje y actualiza la actividad
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } while (!this.fin);
+    }
+
+    private void verificarTimeout() {
+        // Hilo para verificar si el servidor no responde
+        while (!fin) {
+            long ahora = System.currentTimeMillis();
+            if (ahora - ultimaActividadServidor > TIMEOUT_SERVIDOR) {
+                System.out.println("Servidor no responde. Desconectado.");
+                desconectarPorTimeout();
+                break;
+            }
+
+            try {
+                Thread.sleep(500); // Revisa cada 500ms
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void procesarMensaje(DatagramPacket packet) {
+        String mensaje = (new String(packet.getData())).trim();
+        ultimaActividadServidor = System.currentTimeMillis();  // Actualiza el tiempo de última actividad
+        String[] partes = mensaje.split(":");
+
+        System.out.println("Mensaje recibido: " + mensaje);
+
+        switch (partes[0]) {
+            case "Yaconectado":
+                System.out.println("Ya estas conectado");
+                break;
+            case "Conectado":
+                System.out.println("Conectado al servidor");
+                this.ipServidor = packet.getAddress();
+                gameController.conectar(Integer.parseInt(partes[1]));
+                break;
+            case "Lleno":
+                System.out.println("Servidor lleno");
+                this.fin = true;
+                break;
+            case "Empezar":
+                this.conexionEntreJugadores = true;
+                this.gameController.empezar(Integer.parseInt(partes[1]), Integer.parseInt(partes[2]));
+                break;
+            case "Nivel":
+                this.gameController.asignarNivel(Integer.parseInt(partes[1]));
+                break;
+            case "UpdateState":
+                this.gameController.actualizarEstado(partes);
+                break;
+            case "Derrota":
+                this.gameController.perder();
+                break;
+            case "Animar":
+                this.gameController.animar(Integer.parseInt(partes[1]), Boolean.parseBoolean(partes[2]),
+                        Boolean.parseBoolean(partes[3]), Boolean.parseBoolean(partes[4]));
+                break;
+            case "Atacar":
+                this.gameController.animarPersonajeAtaque(partes);
+                break;
+            case "MovimientoEnemigos":
+                this.gameController.actualizarMovimientoEnemigos(partes);
+                break;
+            case "BalasEnemigos":
+                this.gameController.actualizarBalasEnemigos(partes);
+                break;
+            case "ActualizarBalas":
+                this.gameController.actualizarPosicionBalas(partes);
+                break;
+            case "CajaRota":
+                this.gameController.eliminarCaja(partes);
+                break;
+            case "BalaImpactada":
+                this.gameController.eliminarBala(partes);
+                break;
+            case "CambioPersonaje":
+                this.gameController.cambiarPersonaje(Integer.parseInt(partes[1]), Integer.parseInt(partes[2]));
+                break;
+            case "NivelCompletado":
+                this.gameController.avanzarNivel(partes);
+                break;
+            case "CambioPersonajesNivel":
+                this.gameController.cambiarPersonajesPorNivel(partes);
+                break;
+            case "Desconectado":
+                this.gameController.volverAlMenu();
+                break;
+            case "Victoria":
+                this.gameController.ganarPartida();
+                break;
+            case "ErrorJugador":
+                System.out.println("El servidor ha desconectado al jugador.");
+                this.gameController.tirarErrorPorDesconexion();
+                break;
+        }
+    }
+
+    public void sendMessage(String message) {
+        byte[] byteMessage = message.getBytes();
+        DatagramPacket packet = new DatagramPacket(byteMessage, byteMessage.length, this.ipServidor, this.servidorPort);
+        try {
+            this.socket.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void finalizar() {
+        this.fin = true;
+        this.socket.close();
+        this.interrupt();
+        System.out.println("Conexión finalizada");
+    }
+
+    public boolean getConexionEntreJugadores() {
+        return this.conexionEntreJugadores;
+    }
+
+    private void desconectarPorTimeout() {
+        if (fin) return;
+        fin = true;
+
+        try {
+            this.socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Avisar al juego sobre la desconexión por timeout
+        Gdx.app.postRunnable(() -> {
+            gameController.tirarErrorPorDesconexion();
+        });
+    }
+}
